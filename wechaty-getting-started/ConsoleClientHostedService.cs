7f09058c0656a5c3521e792c67174e66e2c30a50
zhengbo <zhengbo@7net.cc>
2020-08-16 17:08:06 +0800

    init dotnet wechaty getting started

---
+++ wechaty-getting-started/ConsoleClientHostedService.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Modularity.PlugIns;
+using Wechaty.Application.Contracts;
+using Wechaty.Domain.Shared;
+using Wechaty.Domain.Shared.DTO;
+using Wechaty.PlugIn;
+using Wechaty.PuppetModel;
+
+namespace wechaty_getting_started
+{
+    public class ConsoleClientHostedService : IHostedService
+    {
+        private IWechatyBotService bot { get; set; }
+        private IConfiguration configuration { get; set; }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using (var application = AbpApplicationFactory.Create<WechatyConsoleModule>(options =>
+            {
+                options.UseAutofac();
+                options.Services.AddLogging(loggingBuilder =>
+                {
+                    loggingBuilder.AddSerilog(dispose: true);
+                });
+
+                // 绑定插件，方式一：加载模块 方式二：加载 dll
+                // TODO 这里可以读取所有继承 WechatyPluginBaseModule的模块，动态读取
+                options.PlugInSources.AddTypes(
+                    typeof(WechatyDingDongModule),
+                    typeof(WechatyQRCodeTerminalModule));
+
+            }))
+            {
+                application.Initialize();
+
+                configuration = application.ServiceProvider.GetRequiredService<IConfiguration>();
+
+
+                WechatyOptions wechatyOptions = new WechatyOptions()
+                {
+                    Token = configuration["Wechaty_Token"],
+                    Name = configuration["Wechaty_Name"],
+                    EndPoint = configuration["Wechaty_EndPoint"]
+                };
+
+                // 初始化
+                bot = application.ServiceProvider.GetRequiredService<IWechatyBotService>();
+
+                // 插件服务注册
+                var qrCodePlugin = application.ServiceProvider.GetRequiredService<QRCodeTerminalAppService>();
+                var dingdongPlugin = application.ServiceProvider.GetRequiredService<DingDongAppService>();
+
+                bot.Instance(wechatyOptions);
+
+                // Scan Event Plugin
+                qrCodePlugin.QRCodeTerminalAsAscii();
+
+                // Message Event Plugin
+                dingdongPlugin.DingDong();
+
+                // 直接监听
+                bot.On<EventMessagePayload>(EventEnum.Message, async (eventData) =>
+                {
+                    var Message = bot.Message();
+                    var payload = await Message.Ready(eventData.MessageId);
+
+                    if (payload.Text == "天王盖地虎")
+                    {
+                        await Message.Say("宝塔镇河妖", payload);
+                    }
+                });
+
+                await bot.Start();
+            }
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await bot.Stop();
+            Console.WriteLine("shutdown");
+            // 彻底释放资源
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+}
---
+++ wechaty-getting-started/WechatyConsoleModule.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Autofac;
+using Volo.Abp.EventBus;
+using Volo.Abp.Modularity;
+using Wechaty.Application;
+using Wechaty.Application.Contracts;
+using Wechaty.Domain.Shared.DTO;
+
+namespace wechaty_getting_started
+{
+    [DependsOn(
+        typeof(AbpAutofacModule),
+        typeof(AbpEventBusModule),
+        typeof(WechatyApplicationModule),
+        typeof(WechatyApplicationContractModule)
+        //,typeof(WechatyPluginBaseModule)
+        )]
+    public class WechatyConsoleModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            //var hostingEnvironment = context.Services.GetHostingEnvironment();
+            //var configuration = context.Services.GetConfiguration();
+            //ConfigWechatyOption(configuration);
+        }
+    }
+}
